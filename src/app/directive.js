5977deebdb887e630b2723b62011a24a314e8b6c
clownvary <vary_007@163.com>
2015-08-26 00:31:20 +0800

    scope modify in controller ok

--- src/app/directive.js
+++ src/app/directive.js
@@ -20,6 +20,12 @@ angular.module("direc", [])
             transclude: true,
             scope: {
                 'close2': '&onClose'//通过这种写法就间接的和controller里的已定义的回调挂上勾，如果直接把事件写在模板里，是访问不到controller里的方法的
+            },//scope可以在这定义，也能在controller里修改，但一般不用controller
+            controller:function($scope)
+            {
+            	$scope.getName=function(){
+            		alert('mingc');
+            	}
             },
             //scope:false,.//当然如果这样就可以访问得到
             templateUrl: 'my-dialog-close.tpl.html'
