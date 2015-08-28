0707cdc5d7e75a18d406f796e7f2af6b5f95cfce
clownvary <vary_007@163.com>
2015-08-29 01:21:21 +0800

    karma test is ok

---
+++ test/testService.test.js
@@ -0,0 +1,10 @@
+/**
+ * Created by clownvary on 2015/8/28.
+ * Email:vary_007@163.com
+ */
+describe('TestSer', function () {
+    beforeEach(angular.mock.module('direc'));
+    it("contains spec with an expectation", function() {
+        expect(true).toBe(true);
+    });
+})
---
+++ vendor/angular-mock.js
@@ -0,0 +1,4 @@
+/**
+ * Created by clownvary on 2015/8/29.
+ * Email:vary_007@163.com
+ */
